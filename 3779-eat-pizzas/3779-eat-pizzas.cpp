49eb4df79bafabf7443da86e65a3e5ebc1455e14
Vinod Kumar <125216677+vinod2400@users.noreply.github.com>
2025-08-01 20:54:34 +0530

    Time: 73 ms (76.32%), Space: 180.1 MB (27.2%) - LeetHub

--- 3779-eat-pizzas/3779-eat-pizzas.cpp
+++ 3779-eat-pizzas/3779-eat-pizzas.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    long long maxWeight(vector<int>& p) {
-        sort(p.begin(), p.end()); 
-        int n = p.size();
-        int m = n / 4; 
-        int odd = (m + 1) / 2; 
-        int even = m - odd; 
-        
-        long long sum = 0;
-        int l = n - 1;
-        
-        
-        for (int i = 0; i < odd; i++) {
-            sum += p[l]; 
-            l--; 
+    long long maxWeight(vector<int>& pizzas) {
+        sort(pizzas.begin(),pizzas.end());
+        long long ans = 0;
+        int n = pizzas.size();
+        int m = n/4;
+        if(m==1) return pizzas[n-1];
+        if(m==2) return (pizzas[n-1] + pizzas[n-3]);
+        int odd = 0, even = 0;
+        odd = ceil((double)m / 2);
+
+        even = m - odd;
+        int k = n-1;
+        for(int i = 0 ; i < odd ; i++){
+            ans += pizzas[k];
+            k--;
         }
-        
-       
-        for (int i = 0; i < even; i++) {
-            l--;
-            sum += p[l]; 
-            l--; 
+        for(int i = 0; i<even; i++){
+            k--;
+            ans+=pizzas[k];
+            k--;
         }
-        
-        return sum; 
+        return ans;
     }
 };
